cd36a2195a6233a5f26b895e8ae071bd8e1c3c3d
adityaph-17 <adityahalne17@gmail.com>
2026-02-05 20:21:27 +0530

    Add chapter1 Python programs

---
+++ chapter1/jokes.py
@@ -0,0 +1,7 @@
+import pyjokes
+
+joke = pyjokes.get_joke()
+print(joke)
+
+# comment = "This is a simple joke generator using the pyjokes library." cmd+/
+"""multi line comment must use triple quotes"""
