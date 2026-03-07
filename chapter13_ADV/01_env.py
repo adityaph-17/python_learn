fb2a25788c5b4eb379d03328b718497a2a08a36b
adityaph-17 <adityahalne17@gmail.com>
2026-03-07 09:25:13 +0530

    in this chapter13_adv we learn about virtual enivornment, pip freeze command, lambda, join, format function, process list methods, etc

---
+++ chapter13_ADV/01_env.py
@@ -0,0 +1,16 @@
+# VIRTUAL ENVIRIONMENT
+
+# An environment which is same as the system interpreter but is isolated from the other
+# Python environments on the system.
+
+
+# INSTALLATION
+# To use virtual environments, we write:
+# pip install virtualenv # Install the package
+
+# We create a new environment using:
+# virtualenv myprojectenv # Creates a new venv
+
+# The next step after creating the virtual environment is to activate it.
+
+# We can now use this virtual environment as a separate Python installation.
