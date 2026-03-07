886df6c13036c15afb08a664c48495c58de51063
adityaph-17 <adityahalne17@gmail.com>
2026-03-07 09:26:30 +0530

    in this chapter13_adv we learn about virtual enivornment, pip freeze command, lambda, join, format function, process list methods, etc

---
+++ chapter13_ADV/05_format.py
@@ -0,0 +1,9 @@
+# Formats the values inside the string into a desired output.
+
+a= "{} is a good {}".format("aditya", "boy")
+
+print (a)
+
+a= "{1} is a good {0}".format("aditya", "boy")
+
+print (a)
